5ad9b046c6838b1296d759159772a2cd480274f6
Tigran Arakelyan <traqel@gmail.com>
2019-10-24 22:11:29 +0400

    [fix] auth issue fix

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import {AuthContext} from "../../context/AuthContext";
 
 function Auth() {
@@ -6,9 +6,13 @@ function Auth() {
     let urlParams = window.location.href.split("#token=");
     let token = urlParams[urlParams.length - 1];
     setIsAuthenticated(true);
-    window.localStorage.setItem("token", token);
+    localStorage.setItem("token", token);
     updateAuthToken(token);
-    window.top.location.href = '/';
+
+    useEffect(() => {
+        window.top.location.href = '/';
+    }, []);
+
     return (
         <div className="Auth">
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './assets/index.css';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
-import dotenv from 'dotenv'
 import {initClient} from './helpers/initMessengerSdk';
 import {initCurrentUser} from "./helpers/initCurrentUser";
 
@@ -16,7 +15,6 @@ import {initCurrentUser} from "./helpers/initCurrentUser";
         console.log(e)
     }
 
-    dotenv.config();
     ReactDOM.render(<App/>, document.getElementById('root'));
 })();
 
